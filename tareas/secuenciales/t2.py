012a9b4db6115a6dbf910074b1072c3c2f8fe578
CyRstudent <a1433hsaez@iesmediterraneo.org>
2024-11-04 17:22:24 +0100

    Eliminar comentario innecesario

--- tareas/secuenciales/t2.py
+++ tareas/secuenciales/t2.py
@@ -2,4 +2,3 @@ num1 = int(input("Introduce un número entero: "))
 num2 = int(input("Escribe otro número entero: "))
 resultado = num1 + num2
 print(f'La suma de {num1} y {num2} es: {resultado}')
-'la suma de ' + num1 + 'y '
