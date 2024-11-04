3e5555f7b2be5ef4e87762031c33b13f53937313
CyRstudent <a1433hsaez@iesmediterraneo.org>
2024-11-04 17:46:55 +0100

    Error de comprensión del 8

--- tareas/secuenciales/t8.py
+++ tareas/secuenciales/t8.py
@@ -1,10 +1,10 @@
 nombre = input('Introduce tu nombre: ')
 minus = nombre.lower()
 mayus = nombre.upper()
-inip = nombre.split(' ')
-ini = []
-for i in inip:
-    ini.append(list(i)[0])
-
-inic = "".join(ini).upper()
-print(f'Tu nombre en minúsculas: {minus}\nTu nombre en mayúsculas: {mayus}\nTus iniciales en mayusculas son: {inic}')
+nomaps = nombre.split(' ')
+resfin = []
+for nom in nomaps:
+    nom = nom.title()
+    resfin.append(nom)
+res = ' '.join(resfin)
+print(f'Tu nombre en minúsculas: {minus}\nTu nombre en mayúsculas: {mayus}\nTu nombre con las iniciales en mayusculas: {res}')
