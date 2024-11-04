feed0cfc9c3677fdda68e83403a78d13c8a631d9
CyRstudent <a1433hsaez@iesmediterraneo.org>
2024-11-04 17:27:01 +0100

    Se me olvidó añadirle el signo del euro al 7

--- tareas/secuenciales/t7.py
+++ tareas/secuenciales/t7.py
@@ -2,4 +2,4 @@ barrapandura = int(input('Introduce el número de barras vendidas que no son del
 print('El coste de una barra de pan normal es de 3.49€ y se le hace un descuento del 60% por no ser fresca')
 preciopanduro = 3.49 * 0.4
 preciofinal = barrapandura * preciopanduro
-print(f'El coste total por {barrapandura} barras no frescas es {preciofinal}')
+print(f'El coste total por {barrapandura} barras no frescas es {preciofinal}€')
