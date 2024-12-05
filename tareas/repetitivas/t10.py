397f4758313e7d572068307a8f78b73a311907be
CyRstudent <a1433hsaez@iesmarmediterraneo.org>
2024-12-05 12:55:11 +0100

    docs: Añadir comentarios a los ejercicios

--- tareas/repetitivas/t10.py
+++ tareas/repetitivas/t10.py
@@ -1,16 +1,16 @@
-dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo']
+dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'] #Los días de la semana en orden
 
 n_em = int(input('Introduce el número de empleados'))
 salario = float(input('Introduce el salario en €/hora: '))
 
-horas = 0
-for i in range(7):
-    ans = input(f'Ha trabajado su empleado el {dias[i]}?: (S/N) ').lower()
+horas = 0 #Estas son las horas que ha trabajado un empleado en toda la semana
+for i in range(7): #Repetimos 7 veces...
+    ans = input(f'Ha trabajado su empleado el {dias[i]}?: (S/N) ').lower() #Si el empleado ha trabajado ese mismo día...
     if ans == 's':
-        h = int(input(f'Introduce el número de horas trabajadas el {dias[i]}: '))
-        horas += h
+        h = int(input(f'Introduce el número de horas trabajadas el {dias[i]}: ')) #Le añadimos las horas que haya trabajado en el correspondiente día de la semana
+        horas += h #Se lo sumamos al total de horas
 
-t = salario * horas
+t = salario * horas #La nómina semanal será el salario por las horas
     
 print(f'Sueldo semanal del empleado: {t}')
-print(f'Lo que pagará tu empresa por todos los empleados: {t*n_em}')
+print(f'Lo que pagará tu empresa por todos los empleados: {t*n_em}') #Lo que pagará la empresa por todos los empleados en una semana
