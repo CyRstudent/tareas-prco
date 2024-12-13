c0442da63747d3c9b944b1bdfbb199b36696bd87
CyRstudent <a1433hsaez@iesmarmediterraneo.org>
2024-12-13 19:21:17 +0100

    fix: Ahora cuenta todos los sueldos de todos los empleados

--- tareas/repetitivas/t10.py
+++ tareas/repetitivas/t10.py
@@ -2,15 +2,16 @@ dias = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'
 
 n_em = int(input('Introduce el número de empleados'))
 salario = float(input('Introduce el salario en €/hora: '))
-
-horas = 0 #Estas son las horas que ha trabajado un empleado en toda la semana
-for i in range(7): #Repetimos 7 veces...
-    ans = input(f'Ha trabajado su empleado el {dias[i]}?: (S/N) ').lower() #Si el empleado ha trabajado ese mismo día...
-    if ans == 's':
-        h = int(input(f'Introduce el número de horas trabajadas el {dias[i]}: ')) #Le añadimos las horas que haya trabajado en el correspondiente día de la semana
-        horas += h #Se lo sumamos al total de horas
-
-t = salario * horas #La nómina semanal será el salario por las horas
-    
-print(f'Sueldo semanal del empleado: {t}')
-print(f'Lo que pagará tu empresa por todos los empleados: {t*n_em}') #Lo que pagará la empresa por todos los empleados en una semana
+horas_trabajadores = []
+for j in range(n_em):
+    horas = 0 #Estas son las horas que ha trabajado un empleado en toda la semana
+    for i in range(7): #Repetimos 7 veces...
+        ans = input(f'Ha trabajado su empleado nº{j+1} el {dias[i]}?: (S/N) ').lower() #Si el empleado ha trabajado ese mismo día...
+        if ans == 's':
+            h = int(input(f'Introduce el número de horas trabajadas el {dias[i]}: ')) #Le añadimos las horas que haya trabajado en el correspondiente día de la semana
+            horas += h #Se lo sumamos al total de horas
+    horas_trabajadores.append(horas)
+t = sum(horas_trabajadores)
+for h in range(n_em):
+    print(f'Sueldo del empleado nº{h+1}: {salario * horas_trabajadores[h]}')
+print(f'Lo que pagará tu empresa por todos los empleados: {t*salario}') #Lo que pagará la empresa por todos los empleados en una semana
